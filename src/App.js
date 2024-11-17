0fcd87e69a8737196fa3e7016a3c868d56dcc4fb
rakesh thulla <rakeshthulla2005@gmail.com>
2024-11-17 16:30:49 +0530

    Add components folder, server.js, and updated files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+import Index from './components/Index';
+
+
+import CreateAccount from './components/CreateAccount';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/create" element={<CreateAccount />} />
+                <Route path="/index" element={<Index />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
